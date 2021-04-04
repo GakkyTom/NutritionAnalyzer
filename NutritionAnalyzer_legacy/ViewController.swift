6d1922678cc58a1af42981233f713397c630047b
Tomoya Itagaki <itagaki.tomoya@eversense.co.jp>
2021-04-05 01:03:10 +0900

    Initial Commit

---
+++ NutritionAnalyzer_legacy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NutritionAnalyzer_legacy
+//
+//  Created by 板垣智也 on 2021/04/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
